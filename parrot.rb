93899927dff5511cd488165f482c53b7b8395373
Barrett Anderson <barrettanderson1989@gmail.com>
2020-05-27 02:21:57 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,10 +1,2 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(name="Squawk!")
-  puts(name)
-  name
-end
-
-parrot
-  parrot("Pretty bird")
-  parrot("Good bird")
